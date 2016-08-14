dea2cc3cd2c12c59b11b8c4b2dfd68e99fc95329
Giovanni Bassi <giggio@giggio.net>
2016-08-14 01:42:29 -0300

    Fix .sh install script

--- src/dotnet-commands/install.sh
+++ src/dotnet-commands/install.sh
@@ -2,7 +2,7 @@
 releases=$(curl https://github.com/Lambda3/dotnet-commands/releases.atom)
 uri="https://github.com/Lambda3/dotnet-commands/releases/download/$(echo $releases | grep -oPm1 "(?<=<title>)[^<]+" | sed -n 2p)/dotnet-commands.tar.gz"
 outFile=`tempfile`
-curl -o $outFile $uri
+curl -o $outFile -L $uri
 outDir="$outFile-extracted"
 echo $outDir
 mkdir $outDir
@@ -15,5 +15,5 @@ if [ $? -ne 0 ]; then
 fi
 if [[ $PATH != *"$HOME/.nuget/commands/bin"* ]]; then
     echo "PATH=$HOME/.nuget/commands/bin:$PATH" >> $HOME/.bashrc
-    echo "Your .bashrc was updated, either start a new bash instance from scratch or `source ~/.bashrc`."
+    echo "Your .bashrc was updated, either start a new bash instance from scratch or \`source ~/.bashrc\`."
 fi
